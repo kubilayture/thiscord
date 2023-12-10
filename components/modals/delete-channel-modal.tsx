841baf471a8a536b9e852252af2394444dc81edb
Kubilay Türe <kubilayture@gmail.com>
2023-12-10 14:37:26 +0300

    delete channel

--- components/modals/delete-channel-modal.tsx
+++ components/modals/delete-channel-modal.tsx
@@ -38,8 +38,8 @@ export const DeleteChannelModal = () => {
       await axios.delete(url)
 
       onClose()
-      router.refresh()
       router.push(`/servers/${server?.id}`)
+      router.refresh()
     } catch (error) {
       console.log(error)
     } finally {

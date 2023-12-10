5a04ea15d5c6e4be2c95ae2dd6a4744ef88e4192
Kubilay Türe <kubilayture@gmail.com>
2023-12-10 14:52:41 +0300

    edit channel route

--- components/modals/edit-channel-modal.tsx
+++ components/modals/edit-channel-modal.tsx
@@ -80,7 +80,7 @@ export const EditChannelModal = () => {
           serverId: server?.id
         }
       })
-      await axios.post(url, values)
+      await axios.patch(url, values)
 
       form.reset()
       router.refresh()

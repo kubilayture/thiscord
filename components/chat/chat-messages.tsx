ecbc1fca2eeee302ea3dd233485c8958dbb5e1be
Kubilay Türe <kubilayture@gmail.com>
2024-01-20 20:09:51 +0300

    next version dowgraded to 13.4.12 because of socket io incompatibility

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { useChatQuery } from '@/hooks/use-chat-query'
 import { Loader2, ServerCrash } from 'lucide-react'
 import { ChatWelcome } from './chat-welcome'
 import { ChatItem } from './chat-item'
+import { useChatSocket } from '@/hooks/use-chat-socket'
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
@@ -40,6 +41,8 @@ export const ChatMessages = ({
   type
 }: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`
+  const addKey = `chat:${chatId}:messages`
+  const updateKey = `chat:${chatId}:messages:update`
   const {
     data,
     fetchNextPage,
@@ -52,6 +55,7 @@ export const ChatMessages = ({
     paramKey,
     paramValue
   })
+  useChatSocket({ queryKey, addKey, updateKey })
 
   if (status === 'pending') {
     return (
